024a42bd1452ebf0dde6712e8d153d87e911cecb
Stephan Hochhaus <stephan@yauh.de>
2015-05-09 13:38:40 +0200

    ability to add hosts
    rudimentary functionality in place

--- app/collections/HostsCollection.js
+++ app/collections/HostsCollection.js
@@ -1,1 +1,22 @@
 HostsCollection = new Mongo.Collection('hosts');
+HostsCollection.attachSchema(new SimpleSchema({
+  hostname: {
+    type: String,
+    label: "hostname",
+    max: 256
+  },
+  ip: {
+    type: String,
+    label: "ip"
+  },
+  status: {
+    type: String,
+    label: "host status",
+    optional: true
+  },
+  lastSeen: {
+    type: Date,
+    label: "Last time host was seen",
+    optional: true
+  }
+}));
