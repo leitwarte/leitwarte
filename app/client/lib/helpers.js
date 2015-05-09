d9d542d4e02c2968adb6b3b189af61126ad66338
Stephan Hochhaus <stephan@yauh.de>
2015-05-10 00:03:49 +0200

    jsbeautify updates

--- app/client/lib/helpers.js
+++ app/client/lib/helpers.js
@@ -1,37 +1,35 @@
-Template.registerHelper('isActiveRoute', function(routeName) {
+Template.registerHelper('isActiveRoute', function (routeName) {
   if (Router.current().route.getName() === routeName) {
     return 'active';
   }
 });
 
-Template.registerHelper('routeIs', function(routeName) {
+Template.registerHelper('routeIs', function (routeName) {
   if (Router.current().route.getName() === routeName) {
     return true;
   }
 });
 
-Template.registerHelper('formatTime', function(timestamp) {
-  if(timestamp === undefined){
+Template.registerHelper('formatTime', function (timestamp) {
+  if (timestamp === undefined) {
     return false;
   };
   var time = moment(timestamp);
   return time.fromNow();
 });
 
-Template.registerHelper('label', function(string) {
-  switch (string)
-  {
-    case 'online':
-      style = 'label-success';
-      break;
-    case 'offline':
-      style = 'label-default';
-      break;
-    default:
-      style = 'warning';
-      break;
+Template.registerHelper('label', function (string) {
+  switch (string) {
+  case 'online':
+    style = 'label-success';
+    break;
+  case 'offline':
+    style = 'label-default';
+    break;
+  default:
+    style = 'warning';
+    break;
   }
   return style;
-
 
 });
