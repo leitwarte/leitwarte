e9127b9e456955143b084bb5fc0a5a4e76762b2d
Stephan Hochhaus <stephan@yauh.de>
2015-05-09 13:24:48 +0200

    add modal for adding hosts

--- app/client/views/hosts/hosts.js
+++ app/client/views/hosts/hosts.js
@@ -9,10 +9,30 @@ Template.hosts.helpers({
   }
 });
 
+// TODO: There must be a more generic/elegant way to show modals
 Template.hosts.events({
   'click button#add-host': function (evt,tpl) {
     console.log('show Modal');
-    // TODO: Modal is not showing, only HTML gets rendered
     tpl.showModal.set(true);
+  },
+  'click button.close-modal': function (evt,tpl) {
+    console.log('close Modal');
+    tpl.showModal.set(false);
   }
+});
+
+// the modal template callbacks
+// without those the modal is rendered to DOM but not visible
+Template.hostForm.onRendered(function(){
+  console.log("hostForm template created");
+  this.autorun(function(){
+    $('.modal').modal('show');
+  });
+});
+
+Template.hostForm.onDestroyed(function(){
+  console.log("hostForm template destroyed");
+  this.autorun(function(){
+    $('.modal').modal('hide');
+  });
 });
