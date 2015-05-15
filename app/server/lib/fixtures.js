013c3132f0f2e54423355e69e907cb646af70f90
Devon Barrett <devon@devonbarrett.net>
2015-05-15 16:53:17 +0100

    fixes issue with fixtures not being inserted on startup

--- app/server/lib/fixtures.js
+++ app/server/lib/fixtures.js
@@ -1,67 +1,56 @@
-var config = [
-  {
+Meteor.startup( function () {
+  var config = [ {
     _id: 'default',
     checkInterval: 60000
-  }
-];
+  } ];
 
-var apps = [
-  {
+  var apps = [ {
     name: 'todos',
     version: '1.0.0'
-  },
-  {
+  }, {
     name: 'leitwarte',
     version: '0.0.1-dev'
-  }
-];
+  } ];
 
-var hosts = [
-  {
+  var hosts = [ {
     hostname: 'proxy',
     ip: '192.168.2.74',
     status: 'offline'
-  },
-  {
+  }, {
     hostname: 'docker1',
     ip: '192.168.2.72',
     status: 'offline'
-  },
-  {
+  }, {
     hostname: 'cluster1',
     ip: '192.168.2.69',
     status: 'offline'
-  },
-  {
+  }, {
     hostname: 'cluster2',
     ip: '192.168.2.70',
     status: 'offline'
-  },
-  {
+  }, {
     hostname: 'docker2',
     ip: '192.168.2.73',
     status: 'offline'
-  }
-];
-
-Meteor.startup(function () {
-  if (HostsCollection.find().count() === 0) {
-    while (hosts.length > 0) {
-      HostsCollection.insert(hosts.pop());
+  } ];
+  
+  if ( HostsCollection.find().count() === 0 ) {
+    while ( hosts.length > 0 ) {
+      HostsCollection.insert( hosts.pop() );
     }
-    console.log('Added host fixtures');
+    console.log( 'Added host fixtures' );
   }
-  if (AppsCollection.find().count() === 0) {
-    while (apps.length > 0) {
-      AppsCollection.insert(apps.pop());
+  if ( AppsCollection.find().count() === 0 ) {
+    while ( apps.length > 0 ) {
+      AppsCollection.insert( apps.pop() );
     }
-    console.log('Added apps fixtures');
+    console.log( 'Added apps fixtures' );
   }
-  if (ConfigurationCollection.find().count() === 0) {
-    while (config.length > 0) {
-      ConfigurationCollection.insert(config.pop());
+  if ( ConfigurationCollection.find().count() === 0 ) {
+    while ( config.length > 0 ) {
+      ConfigurationCollection.insert( config.pop() );
     }
-    console.log('Added default configuration settings');
+    console.log( 'Added default configuration settings' );
   }
 
-});
+} );
