09c88fffc0c3643ec871a6898e4aa001725a7b57
C W <carl@wolsey.org>
2015-05-15 13:08:12 +0100

    Removed meteorhacks:npm

--- app/server/methods/ping.js
+++ app/server/methods/ping.js
@@ -1,17 +1,3 @@
-var pingWrapped = function(host){
-  var Future = Npm.require('fibers/future');
-  var ping = Meteor.npmRequire('jjg-ping')
-  var future = new Future();
-  
-  ping.system.ping(host, function(latency, status){
-    var statusText = "offline";
-    status ? statusText = "online" : statusText = "offline"  
-    future.return({latency: latency, online: status, status: statusText});
-  });
-
-  return future.wait();
-}
-
 Meteor.methods({
   'pingHost': function(host){
     this.unblock();
